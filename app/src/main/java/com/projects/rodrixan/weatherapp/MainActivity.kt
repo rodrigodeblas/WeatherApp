98bea205406a024084c5b39069be8bd786490666
Rodrigo de Blas García <r.de.blas.garcia@accenture.com>
2018-08-16 09:54:22 +0200

    Project creation

---
+++ app/src/main/java/com/projects/rodrixan/weatherapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.projects.rodrixan.weatherapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
