9235afcf9cf0daf2f6fcb33baf5cd7144d213c25
Rodri de Blas <rodri.xan@gmail.com>
2018-09-03 10:19:47 +0200

    Added insert functions

--- app/src/main/java/com/projects/rodrixan/weatherapp/base/room/ForecastDataDao.kt
+++ app/src/main/java/com/projects/rodrixan/weatherapp/base/room/ForecastDataDao.kt
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
 
+
 @Dao
 interface ForecastDataDao {
     @Query("SELECT * from DayForecast")
@@ -12,6 +13,12 @@ interface ForecastDataDao {
 
     @Insert(onConflict = REPLACE)
     fun updateDayForecast(dayForecast: DayForecast)
+
+    @Insert(onConflict = REPLACE)
+    fun updateCityForecast(cityForecast: CityForecast)
+
+    @Insert(onConflict = REPLACE)
+    fun insertDayForecasts(dayForecasts: List<DayForecast>)
 
     @Query("DELETE from DayForecast")
     fun deleteAll()
